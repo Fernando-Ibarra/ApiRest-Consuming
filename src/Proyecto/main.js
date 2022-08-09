88cdc3b6365805de908596a8f1be7af6111e6f2f
Fernando-Ibarra <fi94457@gmail.com>
2022-08-09 11:35:50 -0600

    Dia 3

--- src/Proyecto/main.js
+++ src/Proyecto/main.js
@@ -1,15 +1,54 @@
-const API = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=0bcad1ba-f0f3-470a-b522-db98ddac5976';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=0bcad1ba-f0f3-470a-b522-db98ddac5976';
 
-async function reload() {
-    const res = await fetch(API);
+const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=0bcad1ba-f0f3-470a-b522-db98ddac5976';
+
+const spanError = document.getElementById('error');
+
+async function loadRandomMichis() {
+    const res = await fetch(API_URL_RANDOM);
     const data = await res.json();
+    console.log('Random');
     console.log(data);
-    const img1 = document.getElementById('img1');
-    const img2 = document.getElementById('img2');
-    const img3 = document.getElementById('img3');
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error: " + res.status;
+    } else {
+        const img1 = document.getElementById('img1');
+        const img2 = document.getElementById('img2');
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    }
 };
 
-reload();
+async function loadFavouritesMichis() {
+    const res = await fetch(API_URL_FAVOURITES);
+    const data = await res.json();
+    console.log('Favoritos');
+    console.log(data);
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error - " + res.status + " : " + data.message;
+    } else {
+    }
+};
+
+async function saveFavouriteMichis() {
+    const res = await fetch(API_URL_FAVOURITES, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            image_id: 'MTg2ODc5Mw',
+        }),
+    });
+    const data = await res.json();
+    console.log('Save');
+    console.log(res);
+
+    if (res.status !== 200) {
+        spanError.innerHTML = "Hubo un error - " + res.status + " : " + data.message;
+    } else {
+    }
+}
+
+loadRandomMichis();
+loadFavouritesMichis();
